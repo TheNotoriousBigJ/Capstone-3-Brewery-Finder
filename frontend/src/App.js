2472db31cc8e94d182c9364782462dfcbe5d3e0e
Duran Brehon <duranbrehon@gmail.com>
2022-03-02 18:38:08 -0500

    Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,20 @@
 import Main from './Components/Main/Main'
 import {Provider} from 'react-redux'
+import React from 'react'
 import {BrowserRouter} from 'react-router-dom'
 import {ConfigureStore} from './Redux/configureStore'
+import Navbar from './Components/Navbar'
+import { Nav } from 'react-bootstrap'
 
 const store = ConfigureStore();
 
 function App() {
+ 
 
-  return (
+ return (
     <Provider store={store}>
+
+      <Navbar></Navbar>
       <BrowserRouter>
         <Main/>
       </BrowserRouter>
