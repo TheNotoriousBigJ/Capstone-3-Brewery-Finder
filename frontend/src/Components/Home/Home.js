8e4fd79ca49d2ddf5020ea26b6101467fc016d79
Duran Brehon <duranbrehon@gmail.com>
2022-03-10 19:51:21 -0500

    React New Repo

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -1,11 +1,38 @@
-import {Link} from 'react-router-dom'
+import React from 'react'
+import { Container, Col } from 'react-bootstrap'
+import './Home.css'
 
-const Home = (props) => {
-    return(
-        <div>
-            You must be authorized to see this page.
-        </div>
+const Home = () => {
+    return (
+        <>
+        <style type="text/css">
+            {`
+            .col-1 {
+                padding: 1rem 1rem 1rem 0;
+                height: 70vh;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                color: white;
+            }
+            `}
+            </style>
+            <Container fluid>
+
+        <Col variant="primary-color" className='home'>
+            <Col className="container">
+                <div className="content">
+                    <div className="text">
+                        <p>Welcome to Bruser</p>
+                    </div>
+                    <h1>Maine's Premier</h1>
+                    <h1>Brewery</h1>
+                </div>
+            </Col>
+        </Col>
+        </Container>
+        </>
     )
 }
 
-export default Home;
+export default Home
